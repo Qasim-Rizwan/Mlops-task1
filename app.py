59aa4f78584e70bc01db237bce27a33d3b75fbb7
Qasim-Rizwan <qasimrizwan16@gmail.com>
2025-02-19 17:18:25 +0500

    first commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
             <title>Simple Flask App</title>
         </head>
         <body>
-            <h1>ITS ME YA BOY CYAN</h1>
+            <h1>ITS me Qasim</h1>
             <p>VERCEL SCENES!!!.</p>
         </body>
         </html>
